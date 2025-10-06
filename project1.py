bd9a6138b1471d9663601705a4d54a51df03b27e
eltuttle2 <eltuttle@umich.edu>
2025-10-06 17:56:14 -0400

    Added CSV reading function and average bill length function

---
+++ project1.py
@@ -0,0 +1,46 @@
+# Name: Elise Tuttle
+# Student ID: 93100225
+# Email: eltuttle@umich.edu
+# GenAI: Used ChatGPT for help with setup, understanding code, and clarifying
+
+import csv
+
+def read_csv_file(filename):
+    """
+    reads a csv and returns a list of dictionaries
+    """
+    data = []
+    with open(filename, newline='', encoding="utf-8") as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            data.append(row)
+    return data
+
+def average_bill_length_per_species(data):
+    """
+    calculates average bill length per species
+    """
+    species_totals = {}
+    species_counts = {}
+
+    for row in data:
+        species = row['species']
+        bill_length = row['bill_length_mm']
+        if species == '' or bill_length == '':
+            continue
+        bill_length = float(bill_length)
+        if species in species_totals:
+            species_totals[species] += bill_length
+            species_counts[species] += 1
+        else:
+            species_totals[species] = bill_length
+            species_counts[species] = 1
+
+    averages = {}
+    for species in species_totals:
+        averages[species] = species_totals[species] / species_counts[species]
+
+    return averages
+
+if __name__ == "__main__":
+    main()
